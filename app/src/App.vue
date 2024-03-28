2ed95650c779117d613a4df0c73a0ffbcf9fc877
Kamil Cholewa <kcholew4@gmail.com>
2024-03-28 02:03:09 +0100

    Navbar style change

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,17 +4,44 @@ import { RouterLink, RouterView } from 'vue-router';
 
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
+    <div class="container">
+      <nav>
+        <RouterLink to="/" activeClass="active-route">#Home</RouterLink>
+        <RouterLink to="/about" activeClass="active-route">#About</RouterLink>
+      </nav>
+    </div>
   </header>
 
-  <RouterView />
+  <div class="content">
+    <RouterView />
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 nav > * + * {
   margin-left: 10px;
+}
+
+nav {
+  padding: 2em;
+  background-color: black;
+
+  a {
+    font-family: 'Noto Sans Mono', monospace;
+    font-size: 1rem;
+    color: inherit;
+  }
+
+  .active-route {
+    // color: #fffe88;
+    color: white;
+    font-weight: 600;
+  }
+}
+
+.content {
+  padding: 2em;
+  padding-left: 2em;
+  padding-right: 2em;
 }
 </style>
